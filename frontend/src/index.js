f116303f6973b409c1f5780052d2dab64d4d6bfa
dzazef <dzazef@gmail.com>
2020-04-13 02:57:51 +0200

    Add assets

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './style/index.css';
+import './style/common.css'
 import App from './components/main/App';
 import * as serviceWorker from './serviceWorker';
 

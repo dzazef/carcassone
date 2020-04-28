c8dfa0e2ba7e9de1df5eaef68e5e7a134def6d35
dzazef <dzazef@gmail.com>
2020-04-29 00:03:38 +0200

    Add BoardFunctional

---
+++ frontend/src/components/common/board/BoardFunctional.js
@@ -0,0 +1,13 @@
+import Board from "./Board";
+import {connect} from "react-redux";
+
+const mapStateToProps = (state) => {
+    //TODO: Implement
+}
+
+const mapDispatchToProps = (dispatch) => {
+    //TODO: Implement
+}
+
+const BoardFunctional = connect(mapStateToProps, mapDispatchToProps)(Board)
+export default BoardFunctional

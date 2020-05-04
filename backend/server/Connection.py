5921a9944a00844c5e3fe9ecddab07977083262b
Mateusz Kochanek <mateuszkochanek01@gmail.com>
2020-05-04 21:56:06 +0200

    Implement server and connection classes

--- backend/server/Connection.py
+++ backend/server/Connection.py
@@ -1,14 +1,24 @@
+import asyncio
+import json
+import logging
+import websockets
 from abc import ABC, abstractmethod
 
 class Connection(ABC):
-    def __init__(self):
-        self.id = 0
-
     def run(self):
-        pass #for implementation
+        start_server = websockets.serve(self.connect, "localhost", 6789)
+        asyncio.get_event_loop().run_until_complete(start_server)
+        asyncio.get_event_loop().run_forever()
 
     async def connect(self, websocket, path):
-        pass #for implementation
+        await self.register(websocket)
+        try:
+            async for message in websocket:
+                incomingJson = json.loads(self,message)
+                self.analyze(incomingJson)
+        finally:
+            #tworzenie jsona że ktos sie zerwal i wyslanie go do analyze - command factory
+            await self.analyze(websocket)
 
-    async def analyze(self):
+    async def analyze(self,data,websocket):
         pass #abstract
--- backend/server/Server.py
+++ backend/server/Server.py
@@ -1,11 +1,19 @@
-from abc import ABC, abstractmethod
-from .Connection import Connection
+from backend.server.Connection import Connection
+from backend.command.CommandFactory import CommandFactory
+from backend.game.Game import Game
+
+
 class Server (Connection):
 
     def main(self):#server start
-        self.game
-        self.factory# for implementation
+        self.game = Game()
+        self.factory = CommandFactory()# for implementation
+        self.run()
 
-    async def analyze(self):
-        pass #for implementation
+    async def analyze(self,data,websocket):
+        command = self.factory.createCommand(self.game,data,websocket)
+        sendBackJsonTab = command.execute()
+        for data in sendBackJsonTab:
+            for ws, message in data.items():
+                ws.send(message)
 

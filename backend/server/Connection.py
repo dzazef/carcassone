5ef6153419eaf836eb81ac7929fc230bba80f2ee
Mateusz Kochanek <mateuszkochanek01@gmail.com>
2020-05-05 17:17:05 +0200

    Merge corrections

--- backend/server/Connection.py
+++ backend/server/Connection.py
@@ -1,14 +1,24 @@
+import asyncio
+import json
+import logging
+import websockets
 from abc import ABC, abstractmethod
 
 class Connection(ABC):
-    def __init__(self):
-        self.id = 0
-
     def run(self):
-        pass #for implementation
+        print("ayaya")
+        start_server = websockets.serve(self.connect, "localhost", 6789)
+        asyncio.get_event_loop().run_until_complete(start_server)
+        asyncio.get_event_loop().run_forever()
 
     async def connect(self, websocket, path):
-        pass #for implementation
+        try:
+            async for message in websocket:
+                incomingJson = json.loads(message)
+                await self.analyze(incomingJson,websocket)
+        finally:
+            emptyJson = ""#tworzenie jsona że ktos sie zerwal i wyslanie go do analyze - command factory
+            await self.analyze(emptyJson,websocket)
 
-    async def analyze(self):
+    async def analyze(self,data,websocket):
         pass #abstract

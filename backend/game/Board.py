20ec0174ce08b275009b618c6daaaadd1354a750
Mateusz Kochanek <mateuszkochanek01@gmail.com>
2020-05-13 23:24:11 +0200

    Implements putTile and getTiles methods

--- backend/game/Board.py
+++ backend/game/Board.py
@@ -1,12 +1,24 @@
 class Board:
     def __init__(self):
-        pass
+        self.tile_matrix = [[None for i in range(150)] for i in range(150)]
 
     def getTiles(self):
-        pass
+        return self.tile_matrix
 
     def putTile(self, tile, x, y):
-        pass
+        if y-1 >= 0 and self.tile_matrix[x][y-1] != None:
+            tile.leftTile = self.tile_matrix[x][y-1]
+            (self.tile_matrix[x][y-1]).rightTile = tile
+        if x-1 >= 0 and self.tile_matrix[x-1][y] != None:
+            tile.upTile = self.tile_matrix[x-1][y]
+            (self.tile_matrix[x-1][y]).downTile = tile
+        if y+1 <= 149 and self.tile_matrix[x][y+1] != None:
+            tile.rightTile = self.tile_matrix[x][y+1]
+            (self.tile_matrix[x][y+1]).leftTile = tile
+        if x+1 <= 149 and self.tile_matrix[x+1][y] != None:
+            tile.downTile = self.tile_matrix[x+1][y]
+            (self.tile_matrix[x+1][y]).uptTile = tile
+        self.tile_matrix[x][y] = tile
 
     def putPawn(self, tile, color, x, y):
         pass

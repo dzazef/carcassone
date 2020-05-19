d53c43cf83b30077c177a715702f04a51b481ad5
Mateusz Kochanek <mateuszkochanek01@gmail.com>
2020-05-19 22:29:12 +0200

    Implements end game functionality within EndTurnCommand

--- backend/command/EndTurnCommand.py
+++ backend/command/EndTurnCommand.py
@@ -1,3 +1,7 @@
+from json import dumps
+from operator import itemgetter
+
+import JSONConstructor
 from backend.command.Command import Command
 
 
@@ -6,4 +10,14 @@ class EndTurnCommand(Command):
         super(EndTurnCommand, self).__init__(game, data)
 
     def execute(self):
-        pass
+        nextTurn = self._game.nextTurn()
+        players = self._game.getPlayers()
+        if nextTurn:
+            pass
+        else: # end the game | winners = [(place1, id1, points1), (place2, id2, points2)]
+            winners = [(0,p.getId(),p.getPoints()) for p in players]
+            sorted(winners, key=itemgetter(2))
+            for i in range(len(players)):
+                winners[i][0] = i+1
+            json = {p.getWebsocket(): [dumps(JSONConstructor.end_game(winners))] for p in players}
+        return json

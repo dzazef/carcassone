f0efcf7db6dbb4290516933b34ae152e3e272063
244954 <mateusz_m_malecki@interia.pl>
2020-06-05 13:34:02 +0200

    Fixed bug where points failed to be awarded on tiles with multiple roads

--- backend/tile/TileRoad.py
+++ backend/tile/TileRoad.py
@@ -11,32 +11,30 @@ class TileRoad(Tile):
         for i in self.sides:
             if i[1] == Terrains.ROAD:
                 dfs = self.dfs_start(i)
-                for i in dfs:
-                    if i == (None, None):
-                        return "not completed road"
-                how_many_tiles = [j[0] for j in dfs]
-                how_many_tiles2 = list(set(how_many_tiles))
-                points = len(how_many_tiles2)
+                if (None, None) not in dfs:
+                    how_many_tiles = [j[0] for j in dfs]
+                    how_many_tiles2 = list(set(how_many_tiles))
+                    points = len(how_many_tiles2)
 
-                players_list = [j[1][3] for j in dfs]
-                counter2 = []
-                occurence = []
-                for l in players_list:
-                    if l is not None and occurence.count(l) == 0:
-                        occurence.append(l)
-                        counter = players_list.count(l)
-                        counter2.append((l, counter))
-                counter2.sort(key=operator.itemgetter(1), reverse=True)
-                if not counter2:
-                    return {}
-                score[counter2[0][0]] = [points, counter2[0][1]]
-                for c in range(1, len(counter2)):
-                    if counter2[c][1] == counter2[0][1]:
-                        score[counter2[c][0]] = [points, counter2[c][1]]
-                    else:
-                        score[counter2[c][0]] = [0, counter2[c][1]]
-                for j in dfs:  # clear pawns
-                    j[1][3] = None
+                    players_list = [j[1][3] for j in dfs]
+                    counter2 = []
+                    occurence = []
+                    for l in players_list:
+                        if l is not None and occurence.count(l) == 0:
+                            occurence.append(l)
+                            counter = players_list.count(l)
+                            counter2.append((l, counter))
+                    counter2.sort(key=operator.itemgetter(1), reverse=True)
+                    if not counter2:
+                        return {}
+                    score[counter2[0][0]] = [points, counter2[0][1]]
+                    for c in range(1, len(counter2)):
+                        if counter2[c][1] == counter2[0][1]:
+                            score[counter2[c][0]] = [points, counter2[c][1]]
+                        else:
+                            score[counter2[c][0]] = [0, counter2[c][1]]
+                    for j in dfs:  # clear pawns
+                        j[1][3] = None
         return score
 
     def check_for_points_after_game_road(self):

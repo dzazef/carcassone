6b08e458a288117d2d1ebb517de54ebdf19ebc18
dzazef <dzazef@gmail.com>
2020-06-05 22:01:48 +0200

    More meaningful errors

--- frontend/src/ws/wsMiddleware.js
+++ frontend/src/ws/wsMiddleware.js
@@ -24,10 +24,13 @@ const wsMiddleware = () => {
 
     const onClose = store => () => {
         store.dispatch(WSActions.wsDisconnected())
+        if (!store.getState().error?.message) {
+            handleError(store, "Connection broken", {})
+        }
     }
 
     const onError = store => (event) => {
-        handleError(store, "Socket error, isn't the server down?", event)
+        handleError(store, "Not connected", event)
     }
 
     const onMessage = store => (event) => {
